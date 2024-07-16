ea91457720d0c98e60a89621dd8686e91d9f16ff
fmartintbx <facundo.corvalan@mi.unc.edu.ar>
2024-07-16 15:58:44 -0300

    Listing products

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,8 +1,18 @@
+import {  MenuItem } from "../types"
 
+type MenuItemProps = { 
+    item: MenuItem
+}
 
-
-export default function MenuItem() {
+export default function MenuItem({item} : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button  
+    className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between"
+    > 
+       <p>{item.name}</p>
+       <p className="font-black">${item.price}</p>
+    
+    
+    </button>
   )
 }
